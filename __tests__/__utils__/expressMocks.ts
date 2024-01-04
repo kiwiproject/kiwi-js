7650d6914f59cf2bb8eb3b1e6446270312e0dc9d
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2024-01-04 09:55:40 -0500

    Fix bug in standardDeleteResponse to send response (#179)
    Fixes #178

--- __tests__/__utils__/expressMocks.ts
+++ __tests__/__utils__/expressMocks.ts
@@ -3,19 +3,23 @@ import { jest } from "@jest/globals";
 export let res;
 export let resSet;
 export let resStatus;
+export let resSendStatus;
 export let resJson;
 
 export const setupExpressMocks = () => {
   resJson = jest.fn();
   resStatus = jest.fn();
+  resSendStatus = jest.fn();
   resSet = jest.fn();
   res = {
     set: resSet,
     status: resStatus,
+    sendStatus: resSendStatus,
     json: resJson,
   };
 
   resJson.mockImplementation(() => res);
   resStatus.mockImplementation(() => res);
+  resSendStatus.mockImplementation(() => res);
   resSet.mockImplementation(() => res);
 };
